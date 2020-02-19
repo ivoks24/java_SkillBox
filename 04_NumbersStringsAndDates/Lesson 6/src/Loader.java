b66d7134dea348f24d861a32f044b2b9b9d5248a
Uladzislau <ivoks24@gmail.com>
2020-02-20 00:22:23 +0800

    chenged lesson 6

--- 04_NumbersStringsAndDates/Lesson 6/src/Loader.java
+++ 04_NumbersStringsAndDates/Lesson 6/src/Loader.java
@@ -8,16 +8,14 @@ public class Loader {
     public static void main(String[] args) {
 
         Calendar birthday = new GregorianCalendar(1995,Calendar.NOVEMBER,24);
-        DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
-        DateFormat DAY_WEEK = new SimpleDateFormat("EEEE");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd - EEEE");
 
         Calendar now = new GregorianCalendar();
         int i = 0;
 
         for (;;) {
-            System.out.printf("%2d - " + dateFormat.format(birthday.getTime()) + " - %tA%n", i++, birthday.getTime());
-            //System.out.println(MessageFormat.format("{0} - {1} - {2}",
-            //       i, dateFormat.format(birthday.getTime()), DAY_WEEK.format(birthday.getTime())));
+
+            System.out.printf("%n%2d - " + dateFormat.format(birthday.getTime()), i++);
             birthday.add(Calendar.YEAR, 1);
             if (birthday.after(now)) {
                 break;
