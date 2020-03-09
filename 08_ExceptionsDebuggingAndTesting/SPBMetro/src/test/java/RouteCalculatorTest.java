fb9b73fd941e003e23e7570deb1ed931801426f9
Uladzislau <ivoks24@gmail.com>
2020-03-10 00:05:32 +0800

    update test SPBMetro

--- 08_ExceptionsDebuggingAndTesting/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ 08_ExceptionsDebuggingAndTesting/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -21,6 +21,10 @@ public class RouteCalculatorTest extends TestCase {
         line2 = new Line(2, "Вторая");
         line3 = new Line(3, "Третья");
 
+//        stationIndex.addLine(line1);
+//        stationIndex.addLine(line2);
+//        stationIndex.addLine(line3);
+
         route.add(new Station("1.1", line1)); // station 0
         route.add(new Station("1.2", line1)); // station 1
         route.add(new Station("2.1", line2)); // station 2
@@ -28,6 +32,8 @@ public class RouteCalculatorTest extends TestCase {
         route.add(new Station("2.3", line2)); // station 4
         route.add(new Station("3.1", line3)); // station 5
         route.add(new Station("3.2", line3)); // station 6
+
+//        route.forEach(station -> stationIndex.addStation(station));
 
         line1.addStation(route.get(0));
         line1.addStation(route.get(1));
@@ -40,7 +46,6 @@ public class RouteCalculatorTest extends TestCase {
         List<Station> connectionStations1 = new ArrayList<>();
         connectionStations1.add(route.get(1));
         connectionStations1.add(route.get(2));
-
 
         List<Station> connectionStations2 = new ArrayList<>();
         connectionStations2.add(route.get(4));
@@ -57,27 +62,22 @@ public class RouteCalculatorTest extends TestCase {
     public void testGetShortestRoute() {
 
         List<Station> actual;
-        List<Station> expected;
 
         Station st_0 = route.get(0); // station 0; line 1
         Station st_1 = route.get(1); // station 1; line 1
         Station st_4 = route.get(4); // station 4; line 2
         Station st_6 = route.get(6); // station 6; line 3
 
-        actual = routeCalculator.getShortestRoute(st_0, st_1); // one line
-        expected = line1.getStations();
+        actual = routeCalculator.getShortestRoute(st_0, st_1);
+        List<Station> expected = new ArrayList<>(line1.getStations());
         assertEquals(expected, actual);
 
-        actual = routeCalculator.getShortestRoute(st_0, st_4); // one line
-        expected.add(route.get(2));
-        expected.add(route.get(3));
-//        expected.add(route.get(4));
+        actual = routeCalculator.getShortestRoute(st_0, st_4);
+        expected.addAll(line2.getStations());
         assertEquals(expected, actual);
 
-        actual = routeCalculator.getShortestRoute(st_0, st_6); // one line
-        expected.addAll(line3.getStations());
-        assertEquals(expected, actual);
-
+        actual = routeCalculator.getShortestRoute(st_0, st_6);
+        assertEquals(route, actual);
     }
 
     public void testCalculateDuration() {
