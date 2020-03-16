029a10b65bdd200d3428c3dbf372a81262e08ddd
Uladzislau <ivoks24@gmail.com>
2020-03-16 14:35:56 +0800

    changed lesson 3. Table

--- 09_FilesAndNetwork/Lesson 3. Table/src/Main.java
+++ 09_FilesAndNetwork/Lesson 3. Table/src/Main.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public class Main {
 
-    private static HashMap<String, Integer> places = new HashMap<>();
+    private static HashMap<String, Double> allExpenditure = new HashMap<>();
+    private static HashMap<String, Double> allComing = new HashMap<>();
     private static double comingRub = 0;
     private static double expenditureRub = 0;
 
@@ -14,43 +15,61 @@ public class Main {
 
         try {
             List<String> lines = Files.readAllLines(Paths.get("data/movementList.csv"));
+            lines.remove(0);
             lines.forEach(Main::pars);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        places.keySet().forEach(k -> System.out.println(k + " - " + places.get(k)));
+
+        System.out.printf("%nОбщая сумма приходов - %.2f%nСводный отчет о расходе:%n\t===============%n", comingRub);
+        allComing.keySet().forEach(k -> System.out.printf("\t%-13s : %.2f%n", k, allComing.get(k)));
+
+        System.out.printf("%nОбщая сумма расходов - %.2f%nСводный отчет о расходе:%n\t===============%n", expenditureRub);
+        allExpenditure.keySet().forEach(k -> System.out.printf("\t%-13s : %.2f%n", k, allExpenditure.get(k)));
     }
 
     private static void pars(String line) {
 
         String[] str = line.split("\\s+" + " ");
-        if (str.length == 5)
-            if (str[1].length() == 6) {
-                setComingRub(str[3]);
-            } else {
-                setExpenditure(str[1], str[3], str[4]);
-            }
 
+        if (str[1].length() == 6) {
+            setComingRub(str[2], str[3]);
+        } else {
+            setExpenditure(str[1], str[3], str[4]);
+        }
+
+    }
+
+    private static void setComingRub(String place, String money) {
+        String finalPlace = place.substring(place.lastIndexOf("/") + 1);
+        double finalRub = Double.parseDouble(money.substring(money.lastIndexOf(" ") + 1));
+        addPlace(allComing, finalPlace, finalRub);
+        comingRub = comingRub + finalRub;
     }
 
     private static void setExpenditure(String place, String rub, String currency) {
 
-        place = place.substring(place.lastIndexOf("\\") + 1);
-
-        if (!places.containsKey(place)) places.put(place, 1);
-            else places.put(place, places.get(place) + 1);
-
-        if (currency.contains("\"")) rub = conversion(currency);
-
-        expenditureRub = expenditureRub + Double.parseDouble(rub);
+        String finalPlace = place.substring(place.lastIndexOf("\\") + 1);
+        if (currency.contains("\"")) {
+            rub = conversion(currency);
+        }
+        double finalRub = Double.parseDouble(rub);
+        addPlace(allExpenditure, finalPlace, finalRub);
+        expenditureRub = expenditureRub + finalRub;
     }
 
     private static String conversion(String currency) {
-        return currency.substring(currency.indexOf("\"") + 1, currency.lastIndexOf("\"")).replace(",", ".");
+        return currency.substring(currency.indexOf("\"") + 1, currency
+                .lastIndexOf("\"")).replace(",", ".");
     }
 
-    private static void setComingRub(String money) {
-        comingRub = comingRub + Double.parseDouble(money.substring(money.lastIndexOf(" ") + 1));
+    private static void addPlace(HashMap<String, Double> map, String place, Double money) {
+
+        if (!map.containsKey(place)) {
+            map.put(place, money);
+        } else {
+            map.put(place, map.get(place) + money);
+        }
     }
 }
