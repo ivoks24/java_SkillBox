a9a50adee4040f472a51482fe6af7b19a833e130
Uladzislau <ivoks24@gmail.com>
2020-03-24 16:08:15 +0800

    added lesson 1. JDBC

---
+++ 10_SQLAndHibernate/Lesson 1. JDBCExp/src/main/java/Main.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        String url = "jdbc:mysql://localhost:3306/skillbox";
+        String url_2 = "jdbc:mysql://localhost:3306/skillbox"+
+                "?verifyServerCertificate=false"+
+                "&useSSL=false"+
+                "&requireSSL=false"+
+                "&useLegacyDatetimeCode=false"+
+                "&amp"+
+                "&serverTimezone=UTC";
+
+        String user = "root";
+        String pass = "9241026ivoks";
+
+        try {
+            Connection connection = DriverManager.getConnection(url_2, user, pass);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT course_name, COUNT(course_name) / TIMESTAMPDIFF(MONTH, min(subscription_date), max(subscription_date)) AS Month " +
+                            "FROM purchaselist GROUP BY course_name;");
+            while (resultSet.next()) {
+
+//                String courseName = resultSet.getString("name");
+                System.out.println(resultSet.getString(1) + " - " + resultSet.getString(2));
+            }
+            
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
