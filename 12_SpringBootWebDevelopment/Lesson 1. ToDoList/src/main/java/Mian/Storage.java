1c10f27cdce73b2b35c5c88e09473557cad27455
Uladzislau <ivoks24@gmail.com>
2020-04-26 19:37:11 +0800

    m12l2-3

--- 12_SpringBootWebDevelopment/Lesson 1. ToDoList/src/main/java/Mian/Storage.java
+++ 12_SpringBootWebDevelopment/Lesson 1. ToDoList/src/main/java/Mian/Storage.java
@@ -1,48 +1,48 @@
-package Mian;
-
-import model.Task;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-
-public class Storage {
-
-    private static final AtomicInteger currentId = new AtomicInteger(1);
-    private static final ConcurrentHashMap<Integer, Task> tasks = new ConcurrentHashMap<>();
-
-    public static List<Task> getAllTask() {
-        return new ArrayList<>(tasks.values());
-    }
-
-    public static void addTask(Task task) {
-
-        int id = currentId.get();
-        currentId.incrementAndGet();
-
-        task.setId(id);
-        tasks.put(id, task);
-    }
-
-    public static Task getTask(int taskId) {
-        return tasks.getOrDefault(taskId, null);
-    }
-
-    public static boolean update(int taskID, String name, String purpose) {
-
-        Task task = getTask(taskID);
-        if (task != null) {
-            task.setName(name);
-            task.setPurpose(purpose);
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean delete(int taskID) {
-
-        Task task = tasks.remove(taskID);
-        return task != null;
-    }
-}
+//package Mian;
+//
+//import Mian.model.Task;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.concurrent.ConcurrentHashMap;
+//import java.util.concurrent.atomic.AtomicInteger;
+//
+//public class Storage {
+//
+//    private static final AtomicInteger currentId = new AtomicInteger(1);
+//    private static final ConcurrentHashMap<Integer, Task> tasks = new ConcurrentHashMap<>();
+//
+//    public static List<Task> getAllTask() {
+//        return new ArrayList<>(tasks.values());
+//    }
+//
+//    public static void addTask(Task task) {
+//
+//        int id = currentId.get();
+//        currentId.incrementAndGet();
+//
+//        task.setId(id);
+//        tasks.put(id, task);
+//    }
+//
+//    public static Task getTask(int taskId) {
+//        return tasks.getOrDefault(taskId, null);
+//    }
+//
+//    public static boolean update(int taskID, String name, String purpose) {
+//
+//        Task task = getTask(taskID);
+//        if (task != null) {
+//            task.setName(name);
+//            task.setPurpose(purpose);
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    public static boolean delete(int taskID) {
+//
+//        Task task = tasks.remove(taskID);
+//        return task != null;
+//    }
+//}
--- 12_SpringBootWebDevelopment/Lesson 1. ToDoList/src/main/java/Mian/TaskController.java
+++ 12_SpringBootWebDevelopment/Lesson 1. ToDoList/src/main/java/Mian/TaskController.java
@@ -1,18 +1,29 @@
 package Mian;
 
-import model.Task;
+import Mian.model.Task;
+import Mian.model.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TaskController {
 
+    @Autowired
+    private TaskRepository taskRepository;
+
     @GetMapping("/tasks/")
     public synchronized ResponseEntity<List<Task>> list() {
-        List<Task> taskList = Storage.getAllTask();
+
+        Iterable<Task> taskIterable = taskRepository.findAll();
+        ArrayList<Task> taskList = new ArrayList<>();
+
+        taskIterable.forEach(taskList::add);
 
         return !taskList.isEmpty()
                 ? new ResponseEntity<>(taskList, HttpStatus.OK)
@@ -20,19 +31,21 @@ public class TaskController {
     }
 
     @PostMapping("/tasks/")
-    public synchronized ResponseEntity<?> add(Task task) {
+    public synchronized ResponseEntity<?> add(@RequestBody Task task) {
 
-        Storage.addTask(task);
+        taskRepository.save(task);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/tasks/{id}")
     public synchronized ResponseEntity<?> get(@PathVariable int id) {
 
-        Task task = Storage.getTask(id);
-        return (task == null)
-                ? ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
-                : new ResponseEntity<>(task, HttpStatus.OK);
+        Optional<Task> optionalTask = taskRepository.findById(id);
+
+        return (optionalTask.isPresent())
+                ? new ResponseEntity<>(optionalTask.get(), HttpStatus.OK)
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+
     }
 
     @PutMapping("/tasks/{id}")
@@ -40,19 +53,33 @@ public class TaskController {
                                                   @RequestParam String name,
                                                   @RequestParam String purpose) {
 
-        final boolean updated = Storage.update(id, name, purpose);
+        Optional<Task> optionalTask = taskRepository.findById(id);
 
-        return updated
-                ? new ResponseEntity<>(HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+        if (optionalTask.isPresent()) {
+            Task task = optionalTask.get();
+
+            if (!name.equals("")) {
+                task.setName(name);
+            }
+
+            if (!purpose.equals("")) {
+                task.setPurpose(purpose);
+            }
+
+            taskRepository.save(task);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 
     @DeleteMapping("/tasks/{id}")
     public synchronized ResponseEntity<?> delete(@PathVariable int id) {
 
-        final boolean deleted = Storage.delete(id);
-        return deleted
-                ? new ResponseEntity<>(HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+        if (taskRepository.existsById(id)) {
+
+            taskRepository.deleteById(id);
+            return  new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 }
