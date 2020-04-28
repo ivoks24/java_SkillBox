781963b203abd29efc453ec9e9060f579e5125f9
Uladzislau <ivoks24@gmail.com>
2020-04-28 23:23:11 +0800

    m12l5

---
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/Mian/model/Task.java
@@ -0,0 +1,46 @@
+package Mian.model;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Length(min = 1)
+    private String name;
+
+    @Length(min = 1)
+    private String purpose;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+}
